b8f4bacc725f8dc23b6acf0d0d463bf3b96e0295
phanthuy0811 <phanthuy080101@gmail.com>
2026-07-21 12:11:07 +0700

    feat(websocket): add handling websocket

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -21,6 +21,6 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
-  exports: [AuthService, JwtStrategy]
+  exports: [AuthService, JwtStrategy, JwtModule]
 })
 export class AuthModule { }
